d8b8da85de684b81aef2ad09055be521e1aa9b98
Gribs <telekolki@gmail.com>
2022-07-13 19:52:55 +0300

    esbuild code transform added

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,43 @@
 import ReactDom from 'react-dom';
+import { useState, useRef, useEffect } from 'react';
+import * as esbuild from 'esbuild-wasm';
 
 const App = () => {
-    return <h1>Test</h1>
+    const [input, setInput] = useState('');
+    const [code, setCode] = useState('');
+    const esBuildService = useRef<any>();
+
+    const startService = async () => {
+        esBuildService.current = await esbuild.startService({
+            worker: true,
+            wasmURL: '/esbuild.wasm'
+        });
+    };
+
+    useEffect( () => {
+        startService();
+    }, []);
+
+    const onSubmit = async () => {
+        if (!esBuildService) {
+            return;
+        }
+
+        const result = await esBuildService.current.transform(input, {
+            loader: 'jsx',
+            target: 'es2015'
+        })
+
+        setCode(result.code);
+    }
+
+    return <div>
+        <textarea value={input} onChange={(e) => {setInput(e.target.value)}} cols={30} rows={5}></textarea>
+        <div>
+            <button onClick={onSubmit}>Submit</button>
+        </div>
+        <pre>{code ? code : 'Code transpiling result will be here'}</pre>
+    </div>
 };
 
 ReactDom.render(<App/>, document.querySelector('#root'));
